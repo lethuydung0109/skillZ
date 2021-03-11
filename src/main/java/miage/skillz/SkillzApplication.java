24549f5fdec75d6c3424a9d5ab942ff3faecd865
lethuydung0109 <lethuydung0109@gmail.com>
2021-03-11 21:44:16 +0100

    Restructure project

---
+++ src/main/java/miage/skillz/SkillzApplication.java
@@ -0,0 +1,13 @@
+package miage.skillz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miage/skillz/SkillzApplicationTests.java
@@ -0,0 +1,13 @@
+package miage.skillz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
