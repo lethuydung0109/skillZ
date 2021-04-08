d879389528a6fb22b9215d2de4600fcb9ce5897c
riadlazli <riadhlazli@hotmail.com>
2021-04-08 16:39:48 +0200

    travis + sonarqube

---
+++ src/main/java/miage/skillz/controller/CompetenceController.java
@@ -0,0 +1,72 @@
+package miage.skillz.controller;
+
+import miage.skillz.entity.Competence;
+import miage.skillz.service.CompetenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/api")
+public class CompetenceController {
+
+    @Autowired
+    private CompetenceService service;
+
+    // Créer compétence
+    @PostMapping("/all")
+    @RequestMapping(value = "/createCompetence", method = RequestMethod.POST,consumes = "application/json",produces = "application/json")
+    public ResponseEntity<Competence> createCompetence(@RequestBody Competence competence)
+    {
+       // User currentUser = this.userController.findById(userDetails.getId());
+        return service.createCompetence(competence);
+    }
+
+    // Modifier competence
+    @PutMapping("/all")
+    @RequestMapping(value = "/updateCompetence", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
+    public Competence updateCompetence(@RequestBody Competence comp)
+    {
+        return service.updateCompetence(comp);
+    }
+
+    // Supprimer competence
+    @DeleteMapping("/all")
+    @RequestMapping(value = "/deleteComptence/{comptenceId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void deleteCompetence(@PathVariable("comptenceId") Long comptenceId)
+    {
+        service.deleteCompetence(comptenceId);
+    }
+
+    // Liste des compétences
+    @GetMapping("/all")
+    @RequestMapping(value = "/allCompetence", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Competence> getAllCompetence()
+    {
+        return service.getAllCompetence();
+    }
+
+    // Compétence by ID
+    @GetMapping("/all")
+    @RequestMapping(value = "/competenceById/{comptenceId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Competence getCompetenceById(@PathVariable("comptenceId") Long comptenceId)
+    {
+        return service.getCompetenceById(comptenceId);
+    }
+
+    // Compétence by Id Père
+    @GetMapping("/all")
+    @RequestMapping(value = "/getCompetenceByIdPere/{IdPere}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Competence>   getCompetenceIdPere(@PathVariable("IdPere") Long IdPere)
+    {
+        return service.getCompetenceByIdPere(IdPere);
+    }
+
+
+}
+
--- src/main/java/miage/skillz/entity/Competence.java
+++ src/main/java/miage/skillz/entity/Competence.java
@@ -1,5 +1,71 @@
 package miage.skillz.entity;
+import javax.persistence.*;
 
+@Entity
+@Table(	name = "competence")
+/*Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor*/
 public class Competence {
 
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+        // Id de la compétence père
+        private long id_pere;
+
+        // Nom de la compétence
+        private String nom_competence;
+
+        // Mapping hibernate
+        // à completer ....
+
+
+    // Constructeurs     
+    public Competence() {}
+
+    public Competence(long id) {
+        this.id = id;
+    }
+
+    public Competence(String nom_competence) {
+        this.nom_competence = nom_competence;
+    }
+    public Competence(String nom_competence, Long IdPere) {
+        this.nom_competence = nom_competence;
+        this.id_pere = IdPere;
+    }
+
+
+    public Competence(long id, long id_père, String nom_competence) {
+        this.id = id;
+        this.id_pere = id_père;
+        this.nom_competence = nom_competence;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId_pere() {
+        return id_pere;
+    }
+
+    public void setId_pere(long id_pere) {
+        this.id_pere = id_pere;
+    }
+
+    public String getNom_competence() {
+        return nom_competence;
+    }
+
+    public void setNom_competence(String nom_competence) {
+        this.nom_competence = nom_competence;
+    }
 }
