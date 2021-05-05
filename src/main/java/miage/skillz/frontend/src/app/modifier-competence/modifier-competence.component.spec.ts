2b1fcff8678c71fc15ec9eb9435cd91009717274
lethuydung0109 <lethuydung0109@gmail.com>
2021-05-05 16:16:59 +0200

    Reconfig travis + fix error for frontend Job

--- src/main/java/miage/skillz/frontend/src/app/modifier-competence/modifier-competence.component.spec.ts
+++ src/main/java/miage/skillz/frontend/src/app/modifier-competence/modifier-competence.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ModifierCompetenceComponent } from './modifier-competence.component';
+
 
 describe('ModifierCompetenceComponent', () => {
   let component: ModifierCompetenceComponent;
